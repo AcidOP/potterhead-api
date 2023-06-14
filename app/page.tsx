c4b0fee36ab0e5fa78d869ed55c8842af0db452f
Spandan <spandansehgal@gmail.com>
2023-06-14 09:20:17 +0000

    Basic frontend UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navigation from '@/app/components/navigation'
+import Usage from '@/app/components/usage'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Navigation/>
+
+    <span className='lg:text-3xl text-xl font-semibold text-blue-500 flex justify-center mt-3 '>Welcome to PotterHead API</span>
+  
+<Usage/>
+
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,5 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  plugins: [],
+  plugins: [require("daisyui")],
 }
